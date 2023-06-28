567cf72e158aa8cf16980b47c7390e57db195643
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-06-29 02:01:22 +0530

    Filter implemented

--- app/src/main/java/com/example/userhauntedfm/MainActivity.java
+++ app/src/main/java/com/example/userhauntedfm/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.userhauntedfm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -19,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private PlaylistAdapter playlistAdapter;
     private List<Playlist> playlistList;
+    private List<Playlist> filteredList; // List to store filtered results
     private FirebaseFirestore db;
+    private EditText searchEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +35,14 @@ public class MainActivity extends AppCompatActivity {
 
         setupRecyclerView();
         fetchPlaylists();
+        setupSearch();
     }
 
     private void setupRecyclerView() {
         recyclerView = findViewById(R.id.playlistRecyclerView);
         playlistList = new ArrayList<>();
-        playlistAdapter = new PlaylistAdapter(playlistList);
+        filteredList = new ArrayList<>();
+        playlistAdapter = new PlaylistAdapter(filteredList);
 
         // Set the RecyclerView with a GridLayoutManager to display two columns
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
@@ -54,14 +61,14 @@ public class MainActivity extends AppCompatActivity {
                                 playlistList.add(playlist);
                             }
                         }
-                        playlistAdapter.notifyDataSetChanged();
+                        applyFilter(""); // Apply initial filter with empty search query
                     } else {
                         Log.e("MainActivity", "Error fetching playlists: " + task.getException());
                     }
                 });
 
         playlistAdapter.setOnItemClickListener(position -> {
-            Playlist playlist = playlistList.get(position);
+            Playlist playlist = filteredList.get(position);
 
             Intent intent = new Intent(MainActivity.this, SongFetch.class);
             intent.putExtra("playlist", playlist);
@@ -70,4 +77,35 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void setupSearch() {
+        searchEditText = findViewById(R.id.searchEditText);
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Not used
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Apply filter whenever the search text changes
+                applyFilter(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Not used
+            }
+        });
+    }
+
+    private void applyFilter(String query) {
+        filteredList.clear();
+        for (Playlist playlist : playlistList) {
+            // Filter based on playlist name or any other criteria you prefer
+            if (playlist.getName().toLowerCase().contains(query.toLowerCase())) {
+                filteredList.add(playlist);
+            }
+        }
+        playlistAdapter.notifyDataSetChanged();
+    }
 }
